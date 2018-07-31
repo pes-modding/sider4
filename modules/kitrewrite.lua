37d0cd27653c269d4db84f7885b1ab704fe3d29b
Anton Jouline <juce66@gmail.com>
2018-07-31 10:30:35 -0700

    tweaks to kitrewrite.lua module

--- modules/kitrewrite.lua
+++ modules/kitrewrite.lua
@@ -1,15 +1,17 @@
 -- simple kit textures rewrite example:
 -- make the goalkeeper wear 2nd player kit
 
-local function rewrite(ctx, filename)
+local m = {}
+
+function m.rewrite(ctx, filename)
     local team_id = string.match(filename, "\\u(%d+)g1.*%.ftex")
     if team_id then
         return string.gsub(filename, "g1", "p2")
     end
 end
 
-local function init(ctx)
-    ctx.register("livecpk_rewrite", rewrite)
+function m.init(ctx)
+    ctx.register("livecpk_rewrite", m.rewrite)
 end
 
-return { init = init }
+return m
