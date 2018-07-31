4322ac09fe899c1ed66dd2afdef630e2f8eb551c
Anton Jouline <juce66@gmail.com>
2018-07-31 09:53:58 -0700

    doc updates; timeaccel.lua module

--- modules/stadswitch.lua
+++ modules/stadswitch.lua
@@ -2,13 +2,6 @@
 
 local m = {}
 
-function m.set_match_time(ctx, match_time)
-    if ctx.tournament_id == 48 or ctx.tournament_id == 65535 then
-        log("setting match time to 1 minute")
-        return 1
-    end
-end
-
 function m.set_stadium(ctx, options)
     if ctx.tournament_id == 48 and ctx.match_info == 53 then
         -- Konami Cup final
@@ -31,7 +24,6 @@ function m.set_conditions(ctx, options)
 end
 
 function m.init(ctx)
-   ctx.register("set_match_time", m.set_match_time)
    ctx.register("set_stadium", m.set_stadium)
    ctx.register("set_conditions", m.set_conditions)
 end
