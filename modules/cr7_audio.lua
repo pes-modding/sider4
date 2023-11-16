6b2cd81fbbefb53a9a9eed8bd81aed1084b672bf
Anton Jouline <juce66@gmail.com>
2023-11-15 21:43:24 -0800

    minor tweaks to modules

--- modules/cr7_audio.lua
+++ modules/cr7_audio.lua
@@ -13,7 +13,7 @@ function m.data_ready(ctx, filename)
             log("Ronaldo celebration loaded: " .. filename)
             -- Play C.Ronaldo "suuu!" sound, when he scores and does his unique celebration
             -- cr_suuu_d6.mp3 has 6 seconds of silence, followed by "suuu!"
-            cr_sound = audio.new(ctx.sider_dir .. "content\\audio-demo\\cr_suuu_d5.mp3")
+            cr_sound = audio.new(ctx.sider_dir .. "content\\audio-demo\\cr_suuu_d6.mp3")
             cr_sound:set_volume(1)
             cr_sound:play()
             cr_sound:when_done(function()
